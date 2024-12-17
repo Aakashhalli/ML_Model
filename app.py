d0963ea82293a9f54717be4191b8dbc3e961efb2
Akash Halli <118992375+Aakashhalli@users.noreply.github.com>
2024-12-17 23:11:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ def predict():
         'predicted_class': predicted_class,
         'confidence': float(confidence)
     })
-
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=8000)
+
